23314a419f47b8b3a53750417e5b62efad9fed43
Vinícius Sizílio <vinicius.sizilio@gmail.com>
2022-09-13 12:12:52 -0300

    Updates

--- config/core.php
+++ config/core.php
@@ -1,5 +1,9 @@
 <?php
 
+// PHP sets
+set_time_limit(120); // timeout in seconds
+ini_set('display_errors', 0);
+
 // Includes
 require_once 'config.php';
 require_once API_ROOT . '/src/app.php';
@@ -11,7 +15,6 @@ require_once API_ROOT . '/src/objects/client.php';
 require_once API_ROOT . '/src/objects/product.php';
 
 // Error reporting
-ini_set('display_errors', 0);
 if (!API_PROD) {
     ini_set('display_errors', 1);
     error_reporting(E_ALL);
